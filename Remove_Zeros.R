af1818cb413cf47c6f8756c11814f60c6b222368
Ph.C <ph.castonguay@gmail.com>
2015-03-03 10:04:13 -0500

    ok

--- Remove_Zeros.R
+++ Remove_Zeros.R
@@ -1,5 +1,4 @@
 #Remove all genes with zeros in the dataset
 
 anyZ = apply(dat,1,function(x){min(x)!=0})
-
 new_data<-dat[anyZ,]
--- SOM_Test_ggplot.R
+++ SOM_Test_ggplot.R
@@ -1,44 +1,30 @@
-setwd("~/Desktop/SelfOorganizingMaps")
+setwd("~/Desktop/BioHack-CosmiK")
+
+library(kohonen)
+library(class)
+library(MASS)
 
 require(kohonen)
 
 dat = read.table("tcga_RSEM_tranform.csv", header=TRUE, sep="\t", as.is=TRUE)
 
-med.g = apply(dat,1,median)
-hist(med.g, breaks=200)
-max.g = apply(dat,1,max)
-hist(max.g, breaks=200)
-dat.0 = dat[max.g>0,]
+#Removing the genes that contains at least a 0
+anyZero = apply(dat,1,function(x){min(x)!=0})
+dat.0 = dat[anyZero,]
 
 #training set
-dat_subset<-dat.0
-dat_train<-as.matrix(scale(dat_subset))
+dat_train<-as.matrix(scale(dat.0))
 
-som_grid <- somgrid(xdim = 20, ydim=20, topo="hexagonal")
+som_grid <- somgrid(xdim = 40, ydim=40, topo="hexagonal")
 
-som_model <- som(dat_train, grid=som_grid, rlen=100, alpha=c(0.05,0.01), keep.data = TRUE, n.hood="circular" )
+som_model <- som(dat_train, grid=som_grid, rlen=60, alpha=c(0.05,0.01), keep.data = TRUE, n.hood="circular" )
 rm(som_grid, dat_train)
 
 plot(som_model, type="changes")
 plot(som_model, type="count")
-plot(som_model, type="dist.neighbours")
+plot(som_model, type="dist.neighbours", palette.name=grey.colors)
 
 str(som_model)
-
-## Cluster
-som_cluster = cutree(hclust(dist(som_model$codes)),6)
-
-## Effect of expression filters
-med.r = apply(dat.0, 1, median)
-sd.r = apply(dat.0, 1, sd)
-hist(sd.r)
-hist(med.r)
-dat_train = as.matrix(dat.0)[sd.r>.0 & med.r>0.6,1:30]
-som_grid <- somgrid(xdim = 10, ydim=10, topo="hexagonal")
-som_model <- som(dat_train, grid=som_grid, rlen=100, alpha=c(0.05,0.01), keep.data = TRUE, n.hood="circular" )
-plot(som_model, type="count")
-
-
 
 ## ggplot2
 library(ggplot2)
@@ -54,7 +40,7 @@ unit.pdf = data.frame(somId=1:nrow(som_model$grid$pts), som_model$grid$pts,
 pdf = merge(pdf, unit.pdf)
 pdf.s = pdf %>% group_by(x, y, somId, clust) %>% summarize(nb.genes=n(), dist.med=median(dist), med.exp=median(med.exp), sd.exp=mean(sd.exp))
 
-save(pdf, pdf.s, file="somDF-example.RData")
+save(pdf, pdf.s, file="CosmiK.SOM")
 
 ggplot(pdf.s, aes(x=x,y=y)) + geom_point(size=7) + geom_point(aes(colour=nb.genes),size=6) + theme_bw() + scale_colour_gradient(low="red",high="white")
 ggplot(pdf.s, aes(x=x,y=y)) + geom_point(size=7) + geom_point(aes(colour=med.exp),size=6) + theme_bw() + scale_colour_gradient(low="red",high="white")
